4e771e79bce7e6bb7063a9d6ebc77bd50534b254
andreareyes00 <78465028@certus.edu.pe>
2021-06-19 19:04:00 -0500

    Ultimos Detalles

--- subpaginas/main.js
+++ subpaginas/main.js
@@ -3,4 +3,18 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+
+    //Guardamos el modo en localstorage.
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
 });
+
+//obtenemos el modo actual.
+if (localStorage.getItem('dark-mode') --- 'true'){
+    document.body.classList.add('dark');
+} else {
+    document.body.classList.remove('dark');
+}
